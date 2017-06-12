4fa3b8ab9e210d19916467fc812473b428828497
jason <cookie321@gmail.com>
2017-06-12 16:32:20 +0900

    20170612 Dynamic Web with EJS

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, '/public')));
 //console.log(__dirname);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/hello', function (req, res) {
+  res.render("hello", {name:req.query.nameQuery});
+});
+
+app.get("/hello/:nameParam", function(req, res) {
+  res.render("hello", {name:req.params.nameParam});
 });
 
 app.listen(3000, function () {
